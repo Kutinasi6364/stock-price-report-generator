17dff068be897c2e1ea00af9e47278ad2738f6b3
kutinasi6364 <kutinasi6364_0126@yahoo.co.jp>
2022-05-20 22:41:46 +0900

    20220520 22:41

--- blog.py
+++ blog.py
@@ -1,9 +1,36 @@
+import urllib.robotparser
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://kutinasi-hobbyjoy.com/"
+robot_url = "https://kutinasi-hobbyjoy.com/robots.txt"
+crawl_url = "https://kutinasi-hobbyjoy.com/"
 
-htm = requests.get(url)
+# クローリング可能かチェック
+def check_crawling():
+    ur = urllib.robotparser.RobotFileParser()
+    ur.set_url(robot_url)
+    ur.read()
+    result = ur.can_fetch("*", crawl_url)
+    return result
 
-bs = BeautifulSoup(htm.content, "html.parser")
-print(bs)
+
+# URLから必要な情報の抽出
+def scraping():
+    # URLから情報を取得
+    # response = requests.get(crawl_url)
+    # 解析するURLと解析するパーサーを渡す
+    # soup = BeautifulSoup(response.content, "html.parser")
+    soup = BeautifulSoup(open("scraping-sample.html"), "html.parser")
+    article_title = soup.find_all(
+        "a", attrs={"class": "entry-card-wrap a-wrap border-element cf"}
+    )
+
+    for at in article_title:
+        print(at.attrs["title"])
+        print(at.attrs["href"])
+
+
+if check_crawling():
+    scraping()
+else:
+    print("このページはクローリング禁止です")
