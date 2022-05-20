ec02e4ae04951f391d2026fde1a3ecc91c757f2e
kutinasi6364 <kutinasi6364_0126@yahoo.co.jp>
2022-05-20 23:46:10 +0900

    20220520 23:46

--- blog.py
+++ blog.py
@@ -1,6 +1,7 @@
 import urllib.robotparser
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 robot_url = "https://kutinasi-hobbyjoy.com/robots.txt"
 crawl_url = "https://kutinasi-hobbyjoy.com/"
@@ -17,20 +18,30 @@ def check_crawling():
 # URLから必要な情報の抽出
 def scraping():
     # URLから情報を取得
-    # response = requests.get(crawl_url)
+    response = requests.get(crawl_url)
     # 解析するURLと解析するパーサーを渡す
-    # soup = BeautifulSoup(response.content, "html.parser")
-    soup = BeautifulSoup(open("scraping-sample.html"), "html.parser")
+    soup = BeautifulSoup(response.content, "html.parser")
+
     article_title = soup.find_all(
         "a", attrs={"class": "entry-card-wrap a-wrap border-element cf"}
     )
+    return article_title
 
-    for at in article_title:
-        print(at.attrs["title"])
-        print(at.attrs["href"])
+    # for at in article_title:
+    #    print(at.attrs["title"])
+    #    print(at.attrs["href"])
+
+
+def create_csv(article_title):
+    with open("title-url.csv", "w") as cf:
+        cf = csv.writer(cf, delimiter=",")
+        cf.writerow(["記事タイトル", "URL"])
+        for at in article_title:
+            cf.writerow([at.attrs["title"], at.attrs["href"]])
 
 
 if check_crawling():
-    scraping()
+    article_title = scraping()
+    create_csv(article_title)
 else:
     print("このページはクローリング禁止です")
