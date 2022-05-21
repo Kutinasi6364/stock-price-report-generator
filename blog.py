57b6bb9ee845196793a60bc5ac84cdea2b4eccad
kutinasi6364 <kutinasi6364_0126@yahoo.co.jp>
2022-05-21 15:43:35 +0900

    20220521 15:43

--- blog.py
+++ blog.py
@@ -1,47 +1,15 @@
-import urllib.robotparser
-import requests
-from bs4 import BeautifulSoup
+# ライブラリのインポート
+import openpyxl
 import csv
 
-robot_url = "https://kutinasi-hobbyjoy.com/robots.txt"
-crawl_url = "https://kutinasi-hobbyjoy.com/"
+new_book = openpyxl.Workbook()
+active_sheet = new_book.active
+active_sheet.title = "titleurl"
 
-# クローリング可能かチェック
-def check_crawling():
-    ur = urllib.robotparser.RobotFileParser()
-    ur.set_url(robot_url)
-    ur.read()
-    result = ur.can_fetch("*", crawl_url)
-    return result
+with open("title-url.csv", "r") as tu:
+    tu = csv.reader(tu, delimiter=",")
+    for i, row in enumerate(tu, 1):
+        active_sheet.cell(i, 1, value=row[0])
+        active_sheet.cell(i, 2, value=row[1])
 
-
-# URLから必要な情報の抽出
-def scraping():
-    # URLから情報を取得
-    response = requests.get(crawl_url)
-    # 解析するURLと解析するパーサーを渡す
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    article_title = soup.find_all(
-        "a", attrs={"class": "entry-card-wrap a-wrap border-element cf"}
-    )
-    return article_title
-
-    # for at in article_title:
-    #    print(at.attrs["title"])
-    #    print(at.attrs["href"])
-
-
-def create_csv(article_title):
-    with open("title-url.csv", "w") as cf:
-        cf = csv.writer(cf, delimiter=",")
-        cf.writerow(["記事タイトル", "URL"])
-        for at in article_title:
-            cf.writerow([at.attrs["title"], at.attrs["href"]])
-
-
-if check_crawling():
-    article_title = scraping()
-    create_csv(article_title)
-else:
-    print("このページはクローリング禁止です")
+new_book.save("articletitle.xlsx")
